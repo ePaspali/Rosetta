31823c8584d34f9ff6b190d9e2e8508ecd0b879c
ePaspali <elpi.paspali@gmail.com>
2025-02-18 19:30:47 +0200

    FlexPepDock.sh

--- Docking/FlexPepDock.sh
+++ Docking/FlexPepDock.sh
@@ -1,31 +1,35 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 18 14:28:57 2025
-
-@author: nikipaspali
-"""
-
 #!/bin/bash
-# command to do dockings of template input structures
-#set the full path to the Rosetta database
-DATABASE_PATH="/opt/software/rosetta/2021.16.61629/main/database/"
 
-#set the pattern for input PDB files
-INPUT_PATTERN="tem_*.pdb"
+# set the full path to the Rosetta database
+DATABASE_PATH="//path/to/Rosetta/main/database/"
 
-#specify the options file
+# specify the options file: given bellow
 OPTIONS_FILE="options_flex.inp"
 
-#iterate over the list of input PDB files
-for pdb_file in $INPUT_PATTERN
-do
-    # Set the input PDB file
+# list of specific input PDB files
+INPUT_FILES=("tem_0060.pdb") #replace the input name for all 15 selected template pdbs and repeat this step
+
+# Iterate over the list of input PDB files
+for pdb_file in "${INPUT_FILES[@]}"; do
+    # set the input PDB file
     INPUT_PDB="-s $pdb_file"
 
-    #set the output prefix based on the input file name
+    # set the output prefix based on the input file name
     OUTPUT_PREFIX="-out:prefix flex_$(basename $pdb_file .pdb)"
 
-    #run the FlexPepDocking command
-    /opt/software/rosetta/2021.16.61629/main/source/bin/FlexPepDocking.static.linuxgccrelease $INPUT_PDB $OUTPUT_PREFIX @$OPTIONS_FILE > dock.log
+    # Run the FlexPepDocking command
+    /path/to/Rosetta/main/source/bin/FlexPepDocking.static.linuxgccrelease \
+        $INPUT_PDB $OUTPUT_PREFIX @$OPTIONS_FILE > dock_60.log
 done
+
+##########################################################################
+## options_flex.inp ########
+
+-in:file:spanfile 7BR3_PEP.span -membrane:Membed_init -membrane:Mhbond_depth -score:weights membrane_highres -scorefile score.sc -flexPepDocking:lowres_preoptimize -flexPepDocking:pep_refine
+-ex1
+-ex2aro
+-nstruct 1000
+-use_input_sc
+##########################################################################
+
+
